0b6457155ccae87e9e863ed77cc2b27bfea74041
Charlotte <charlotte@lottia.net>
2024-10-25 17:15:31 +1100

    Imtui cont'd

--- adc/src/Imtui.zig
+++ adc/src/Imtui.zig
@@ -21,6 +21,8 @@ keydown_sym: SDL.Keycode = .unknown,
 keydown_mod: SDL.KeyModifierSet = undefined,
 typematic_on: bool = false,
 
+mouse_row: usize = 0,
+mouse_col: usize = 0,
 mouse_down: ?SDL.MouseButton = null,
 
 // https://ejmastnak.com/tutorials/arch/typematic-rate/
@@ -44,45 +46,50 @@ pub fn processEvent(self: *Imtui, ev: SDL.Event) void {
     switch (ev) {
         .key_down => |key| {
             if (key.is_repeat) return;
-            // try qb.keyDown(key.keycode, key.modifiers);
-            // try qb.keyPress(key.keycode, key.modifiers);
+            try self.handleKeyDown(key.keycode, key.modifiers);
+            try self.handleKeyPress(key.keycode, key.modifiers);
             self.keydown_tick = SDL.getTicks64();
             self.keydown_sym = key.keycode;
             self.keydown_mod = key.modifiers;
             self.typematic_on = false;
         },
         .key_up => |key| {
-            _ = key;
-            // try qb.keyUp(key.keycode);
+            // We don't try to match key down to up.
+            try self.handleKeyUp(key.keycode);
             self.keydown_tick = null;
         },
         .mouse_motion => |motion| {
-            const mouse_x: usize = @intFromFloat(@as(f32, @floatFromInt(motion.x)) / self.scale);
-            const mouse_y: usize = @intFromFloat(@as(f32, @floatFromInt(motion.y)) / self.scale);
-            self.text_mode.positionMouseAt(mouse_x, mouse_y);
-            //     const old_x = qb.mouse_x;
-            //     const old_y = qb.mouse_y;
-            //     if (qb.mouseAt(motion.x, motion.y, scale)) {
-            //         if (mouse_down) |button|
-            //             try qb.mouseDrag(button, old_x, old_y);
-            //         try qb.text_mode.present();
-            //     }
+            const pos = self.interpolateMouse(motion);
+            self.text_mode.positionMouseAt(pos.x, pos.y);
+            if (self.handleMouseAt(self.text_mode.cursor_row, self.text_mode.cursor_col)) |old_loc| {
+                if (self.mouse_down) |button|
+                    try self.handleMouseDrag(button, old_loc.r, old_loc.c);
+            }
         },
-        // .mouse_button_down => |button| {
-        //     if (qb.mouseAt(button.x, button.y, scale))
-        //         try qb.text_mode.present();
-        //     try qb.mouseDown(button.button, button.clicks);
-        //     mouse_down = button.button;
-        // },
-        // .mouse_button_up => |button| {
-        //     if (qb.mouseAt(button.x, button.y, scale))
-        //         try qb.text_mode.present();
-        //     try qb.mouseUp(button.button, button.clicks);
-        //     mouse_down = null;
-        // },
+        .mouse_button_down => |button| {
+            const pos = self.interpolateMouse(button);
+            self.text_mode.positionMouseAt(pos.x, pos.y);
+            _ = self.handleMouseAt(self.text_mode.cursor_row, self.text_mode.cursor_col);
+            try self.handleMouseDown(button.button, button.clicks);
+            self.mouse_down = button.button;
+        },
+        .mouse_button_up => |button| {
+            const pos = self.interpolateMouse(button);
+            self.text_mode.positionMouseAt(pos.x, pos.y);
+            _ = self.handleMouseAt(self.text_mode.cursor_row, self.text_mode.cursor_col);
+            try self.handleMouseUp(button.button, button.clicks);
+            self.mouse_down = null;
+        },
         .quit => self.running = false,
         else => {},
     }
+}
+
+fn interpolateMouse(self: *const Imtui, payload: anytype) struct { x: usize, y: usize } {
+    return .{
+        .x = @intFromFloat(@as(f32, @floatFromInt(payload.x)) / self.scale),
+        .y = @intFromFloat(@as(f32, @floatFromInt(payload.y)) / self.scale),
+    };
 }
 
 pub fn newFrame(self: *Imtui) !void {
@@ -104,4 +111,53 @@ pub fn newFrame(self: *Imtui) !void {
 
 pub fn render(self: *Imtui) !void {
     try self.text_mode.present(self.delta_tick);
+}
+
+fn handleKeyDown(self: *Imtui, keycode: SDL.Keycode, modifiers: SDL.KeyModifierSet) !void {
+    _ = self;
+    _ = keycode;
+    _ = modifiers;
+}
+
+fn handleKeyPress(self: *Imtui, keycode: SDL.Keycode, modifiers: SDL.KeyModifierSet) !void {
+    _ = self;
+    _ = keycode;
+    _ = modifiers;
+}
+
+fn handleKeyUp(self: *Imtui, keycode: SDL.Keycode) !void {
+    _ = self;
+    _ = keycode;
+}
+
+fn handleMouseAt(self: *Imtui, row: usize, col: usize) ?struct { r: usize, c: usize } {
+    const old_mouse_row = self.mouse_row;
+    const old_mouse_col = self.mouse_col;
+
+    self.mouse_row = row;
+    self.mouse_col = col;
+
+    if (old_mouse_row != self.mouse_row or old_mouse_col != self.mouse_col)
+        return .{ .r = old_mouse_row, .c = old_mouse_col };
+
+    return null;
+}
+
+fn handleMouseDrag(self: *Imtui, button: SDL.MouseButton, old_row: usize, old_col: usize) !void {
+    _ = self;
+    _ = button;
+    _ = old_row;
+    _ = old_col;
+}
+
+fn handleMouseDown(self: *Imtui, button: SDL.MouseButton, clicks: u8) !void {
+    _ = self;
+    _ = button;
+    _ = clicks;
+}
+
+fn handleMouseUp(self: *Imtui, button: SDL.MouseButton, clicks: u8) !void {
+    _ = self;
+    _ = button;
+    _ = clicks;
 }
